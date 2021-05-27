66ad929484e6feabe5cf570d2c9cd9d5f837f56b
Robin Mueller <robin.mueller.m@gmail.com>
2021-05-27 20:11:09 +0200

    added path helper and eclipse project files

---
+++ bbb_path_helper.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:"$HOME/beaglebone/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin"
+export CROSS_COMPILE="arm-linux-gnueabihf"
+export BBB_ROOTFS="${HOME}/beaglebone/rootfs"
